c56f1c2dd4f4384065ee6af79dfe9a1e628aa435
Nanichang Katzing <nanipaul68@gmail.com>
2022-03-19 20:58:38 +0100

    added more fixes

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -1,26 +1,6 @@
 @extends('layout.master')
-
 @section('content')
-    <section class="hero-section ptb-100 gradient-overlay" style="background: url('assets/img/header-bg-5.jpeg')no-repeat center center / cover">
-        <div class="hero-bottom-shape-two" style="background: url('assets/img/hero-bottom-shape.svg')no-repeat bottom center"></div>
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-md-8 col-lg-7">
-                    <div class="page-header-content text-white text-center pt-sm-5 pt-md-5 pt-lg-0">
-                        <h1 class="text-white mb-0">Contact Us</h1>
-                        <div class="custom-breadcrumb">
-                            <ol class="breadcrumb d-inline-block bg-transparent list-inline py-0">
-                                <li class="list-inline-item breadcrumb-item"><a href="#">Home</a></li>
-                                <li class="list-inline-item breadcrumb-item active">Contact Us</li>
-                            </ol>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-
-    <section class="contact-us-promo pt-100">
+    <section class="contact-us-promo pt-100" style="margin-top:100px;">
         <div class="container">
             <div class="row">
                 <div class="col-lg-4 col-md-6 col-sm-6">
@@ -30,7 +10,7 @@
                                 <span class="ti-mobile icon-sm color-secondary"></span>
                             </div>
                             <div><h5 class="mb-0">Call Us</h5>
-                                <p class="text-muted mb-0">+123 456-78900</p>
+                                <p class="text-muted mb-0">+234 816 445 9977</p>
                             </div>
                         </div>
                     </div>
@@ -53,7 +33,7 @@
                                 <span class="ti-email icon-sm color-secondary"></span>
                             </div>
                             <div><h5 class="mb-0">Mail Us</h5>
-                                <p class="text-muted mb-0">info@ppp.plateaustate.gov.ng</p></div>
+                                <p class="text-muted mb-0">dg@ppp.plateaustate.gov.ng</p></div>
                         </div>
                     </div>
                 </div>
@@ -84,20 +64,19 @@
                                         <textarea name="message" id="message" class="form-control" rows="7" cols="25" placeholder="Message"></textarea>
                                     </div>
                                 </div>
-                                <div class="col-sm-12 mt-3">
-                                    <button type="submit" class="btn secondary-solid-btn" id="btnContactUs">
+                                <div class="col-sm-12 d-grid gap-2 mt-3">
+                                    <button type="submit" class="btn btn-outline-success" id="btnContactUs">
                                         Send Message
                                     </button>
+                                    <!-- <button type="button" class="btn btn-outline-success">Success</button> -->
                                 </div>
                             </div>
                         </form>
                     </div>
                 </div>
                 <div class="col-md-5">
-                    <div class="contact-us-content">
+                    <div class="contact-us-content" style="padding-top:40px;">
                         <h2>Looking for a our office?</h2>
-                        
-
                         <hr class="my-5">
 
                         <!-- <h5>Our Headquarters</h5> -->
@@ -106,9 +85,13 @@
                             Little Rayfield Jos, Plateau State
                         </address>
                         <br>
-                        <span>Phone: +1234567890123</span> <br>
-                        <span>Email: <a href="mailto:email@yourdomain.com" class="link-color">email@yourdomain.com</a></span>
-
+                        <span>Phone: +234 816 445 9977</span><br>
+                        <span>
+                            Email: 
+                            <a href="mailto:dg@ppp.plateaustate.gov.ng" style="color:#000;">
+                                dg@ppp.plateaustate.gov.ng
+                            </a>
+                        </span>
                     </div>
                 </div>
             </div>
@@ -116,8 +99,6 @@
     </section>
 
     <div class="google-map">
-        <!-- <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3151.83543450937!2d144.953735315922!3d-37.817323442021234!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad65d4c2b349649%3A0xb6899234e561db11!2sEnvato!5e0!3m2!1sen!2sbd!4v1571030260624!5m2!1sen!2sbd" height="450" style="border:0;" allowfullscreen=""></iframe> -->
-        
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3931.00341513272!2d8.89036256479294!3d9.850079192954041!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x10537442bf6658a9%3A0xf3fe15d9f41f7505!2sPlateau%20State%20Government%20House%2C%20930103%2C%20Jos!5e0!3m2!1sen!2sng!4v1646895504671!5m2!1sen!2sng" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
     </div>
 @endsection
